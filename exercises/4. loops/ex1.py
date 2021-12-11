20802795c427fadc026cb8a8dad3bae66f350cbd
שמואל גדמו <shmuel1232122@gmail.com>
2021-12-12 00:09:40 +0200

    added input method

--- exercises/4. loops/ex1.py
+++ exercises/4. loops/ex1.py
@@ -1,7 +1,19 @@
-num1=int(input("enter num1: "))
-num2=int(input("enter num2: "))
-while
-num=8
-if num%2==0:
-  # print("zugi")
-list(range(10,31,2))
+# even , odd
+
+# generate list from two numbers
+def input_method():
+    # TODO: check which number is smaller
+    number1 = int(input("enter a number1"))
+    number2 = int(input("enter a number2"))
+
+    inputed_list = []
+
+    while number1 <= number2:
+        inputed_list.append(number1)
+        number1 += 1
+
+    return inputed_list
+
+# is_even(num): -> bool
+
+# iterate over list and find even numbers
